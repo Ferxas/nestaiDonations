275530a28d7432aecb5e03700893d41ed3875076
ferxas <ferxas321@hotmail.es>
2024-11-22 20:23:47 -0500

    adding components to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import {useEffect, useState} from 'react';
+import api from './services/api';
+import AnalysisTable from './components/AnalysisTable';
+import AgeChart from './components/AgeChart';
+import MonthlyDonationsChart from './components/MonthlyDonationsChart';
+
 
 const App = () => {
+  const [analysisData, setAnalysisData] = useState(null);
+  
+  useEffect(() => {
+    const fetchAnalysisData = async () => {
+      try {
+        const response = await api.get('/analysis');
+        setAnalysisData(response.data);
+      } catch (error) {
+        console.error('Error fetching analysis data:', error);
+      }
+    }
+
+    fetchAnalysisData();
+  }, []);
+
+  if (!analysisData) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <h1>Análisis de donaciones</h1>
+      <AnalysisTable
+      popularInterests={analysisData.popularInterest}
+      totalDonated={analysisData.total_amount_donated}
+      />
+      <AgeChart ageDistribution={analysisData.age_distribution}/>
+
+      <MonthlyDonationsChart MonthlyDonations={analysisData.monthly_donations} />
+    </div>
   )
 }
 
